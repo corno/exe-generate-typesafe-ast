e2c165080347076a4c35c76f6c29185f7b5f85ff
corno <corno.astn.a@schraverus.com>
2022-11-20 11:11:03 +0100

    .

--- test/src/bin/dependencies.ts
+++
@@ -1,8 +0,0 @@
-import * as collation from "res-pareto-collation/dist"
-import * as fs from "res-pareto-filesystem"
-import { Dependencies } from "../imp/createGetTestSet"
-
-export const dependencies: Dependencies = {
-    createWriteStream: fs.createWriteStream,
-    isYinBeforeYang: collation.localeIsYinBeforeYang,
-}
